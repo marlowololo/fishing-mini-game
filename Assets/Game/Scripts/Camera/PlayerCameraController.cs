251be2d2bb67926a5cbe6ed599092aa87305a303
Eon <marlocoorawa@gmail.com>
2021-10-17 12:48:13 +0700

    Setup basic movement, camera and environment

---
+++ Assets/Game/Scripts/Camera/PlayerCameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour {
+    
+    [Header("References")]
+    [SerializeField] private GameObject _player;
+
+    private Vector3 _positionOffset;
+
+    private void Start() {
+        _positionOffset = transform.position;
+    }
+
+
+    private void Update() {
+        transform.position = _player.transform.position + _positionOffset;
+    }
+}
---
+++ Assets/Game/Scripts/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [Header("Settings")]
+    [SerializeField] private float _movementSpeed = 10f;
+
+    [Header("References")]
+    [SerializeField] private GameObject _playerMesh;
+    [SerializeField] private Animator _playerAnimator;
+
+    private int _isMovingStateID;
+    private Vector3 _movementVector = Vector3.zero;
+
+    private void Start() {
+        _isMovingStateID = Animator.StringToHash("IsMoving");
+    }
+
+    private void Update() {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+        _movementVector.Set(xInput, 0, yInput);
+        bool isMoving = xInput != 0 || yInput != 0;
+        _playerAnimator.SetBool(_isMovingStateID, isMoving);
+        if (isMoving) {
+            _playerMesh.transform.rotation = Quaternion.LookRotation(_movementVector);
+            transform.Translate(_movementVector * Time.deltaTime * _movementSpeed);
+        }
+    }
+}
